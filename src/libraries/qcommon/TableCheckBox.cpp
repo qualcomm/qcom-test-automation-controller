30e5c656de3b8d855fee4cc6d26066d1cd55a19a
Amit Kucheria <amit.kucheria@oss.qualcomm.com>
2026-05-01 03:42:09 +0530

    qcommon: select QCheckBox signal compatible with Qt < 6.7 and >= 6.7
    stateChanged(int) is deprecated-as-error in Qt 6.7+ (Debian 12);
    checkStateChanged(Qt::CheckState) was not added until Qt 6.7 and does
    not exist on Ubuntu 24.04's system Qt (6.4). Use QT_VERSION_CHECK to
    select the right signal at compile time.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@oss.qualcomm.com>

--- src/libraries/qcommon/TableCheckBox.cpp
+++ src/libraries/qcommon/TableCheckBox.cpp
@@ -51,7 +51,11 @@ TableCheckBox::TableCheckBox
 	// Initialize the check-box
 	_checkBox = new QCheckBox(this);
 
-	connect(_checkBox, &QCheckBox::checkStateChanged, this, [=, this](bool newState){ emit checkStateChanged(newState); });
+#if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
+	connect(_checkBox, &QCheckBox::checkStateChanged, this, [=, this](Qt::CheckState state){ emit checkStateChanged(state == Qt::Checked); });
+#else
+	connect(_checkBox, &QCheckBox::stateChanged, this, [=, this](int state){ emit checkStateChanged(state == Qt::Checked); });
+#endif
 
 	// Prepare check box layout
 	QHBoxLayout* layoutCheckBox = new QHBoxLayout(this);
