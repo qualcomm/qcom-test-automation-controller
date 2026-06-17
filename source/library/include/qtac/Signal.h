ff6b3050f9e4252411d3890b0cbc907d96eb999e
Forsgren, Bryant <bryantf@qti.qualcomm.com>
2026-06-17 13:47:14 -0700

    Remove author comment from Signal.h
    Change-Id: I84931d4326410ae6512f058437c06f7447700145

--- source/library/include/qtac/Signal.h
+++ source/library/include/qtac/Signal.h
@@ -30,7 +30,6 @@
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Author: Bryant Finney
 // Qt-free multi-subscriber signal template.
 // Replaces Qt signals/slots for the qtac library.
 //
