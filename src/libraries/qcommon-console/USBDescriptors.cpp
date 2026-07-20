facc87e63fdeff1a300bf152dc8782402e576861
Arkojit Sen <91142102+arko816@users.noreply.github.com>
2026-07-20 12:01:22 +0530

    Update USBDescriptors.cpp
    Signed-off-by: Arkojit Sen <91142102+arko816@users.noreply.github.com>

--- src/libraries/qcommon-console/USBDescriptors.cpp
+++ src/libraries/qcommon-console/USBDescriptors.cpp
@@ -231,7 +231,7 @@ bool USBDescriptors::read(QJsonObject& parentLevel)
 					if (catalogData.contains(kRevisionNumber))
 						usbDescriptor._revision = static_cast<quint32>(catalogData[kRevisionNumber].toInt());
 
-		            if (catalogData.contains(kConfigFilePath) && catalogData[kConfigFilePath].isString())
+					if (catalogData.contains(kConfigFilePath) && catalogData[kConfigFilePath].isString())
 						usbDescriptor._configurationFilePath = catalogData[kConfigFilePath].toString().toLatin1();
 
 #ifdef Q_OS_LINUX
