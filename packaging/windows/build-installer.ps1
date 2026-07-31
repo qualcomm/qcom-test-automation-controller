73536d104f9b0dcfeb506e12dc30c93293f43bb4
Sen, Arkojit <arkosen@qti.qualcomm.com>
2026-07-31 17:29:57 +0530

    Minor fix on build-installer script

--- packaging/windows/build-installer.ps1
+++ packaging/windows/build-installer.ps1
@@ -3,7 +3,6 @@
 
 [CmdletBinding()]
 param(
-    [ValidateSet('x64', 'arm64')] [string] $Arch,
     [string] $SourceRoot,        
     [string] $BinDir,            
     [string] $SevenZipDir,       
@@ -14,9 +13,7 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-if (-not $Arch) {
-    $Arch = if ($env:PROCESSOR_ARCHITECTURE -eq 'ARM64') { 'arm64' } else { 'x64' }
-}
+$Arch = if ($env:PROCESSOR_ARCHITECTURE -eq 'ARM64') { 'arm64' } else { 'x64' }
 
 $here = $PSScriptRoot
 if (-not $here) { $here = Split-Path -Parent $MyInvocation.MyCommand.Path }
